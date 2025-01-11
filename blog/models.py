0154c646749d1bb70793a26af48622e6f3f298f4
Mohamed El Ajouaoui <Mohamedelajouaoui99@gmail.com>
2025-01-11 23:48:04 +0100

    Define model for articles
    Foreign key is currently unnecessary might be later on
    important when adding the fields categorie and tags.
    
    All fields should be rendert inside the template by defining
    the contributing meta.

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Article(models.Model):
+    author = models.CharField(max_length=20)
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+    release_date = models.DateField(default=date.today)
