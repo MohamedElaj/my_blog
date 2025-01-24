b1753e1ca25e1e99568554e237c985c916b2305b
Mohamed El Ajouaoui <Mohamedelajouaoui99@gmail.com>
2025-01-24 21:24:05 +0100

    Add a page for the content in each article
    So the Goal is that every article has a specific url which leads
    to to the content of that specific article. This makes reading the
    article much more clean.

--- blog/views.py
+++ blog/views.py
@@ -7,3 +7,8 @@ from .models import Article
 def article_list(request):
     articles = Article.objects.all()
     return render(request, "home.html", {"articles": articles})
+
+
+def article_detail(request, pk):
+    articles = get_object_or_404(Article, pk=pk)
+    return render(request, "article_detail.html", {"article": article})
